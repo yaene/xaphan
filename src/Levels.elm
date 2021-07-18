f1de8a43716018e1c2cfdf47f4cacf0efbca84a1
yannis.bolliger <yannis.bolliger@sjtu.edu.cn>
2021-07-18 19:54:26 +0200

    add basic level 2 with two enemies (#21)

--- src/Levels.elm
+++ src/Levels.elm
@@ -7,6 +7,7 @@ import Enemy exposing (Enemy)
 
 type Level
     = Level1
+    | Level2
 
 
 type alias LevelData =
@@ -18,3 +19,8 @@ loadLevel level =
     case level of
         Level1 ->
             [ Enemy ( 50, 50 ) 2 0 Right (Animation 0 1500 False True) (Animation 0 1000 False True) ]
+
+        Level2 ->
+            [ Enemy ( 50, 50 ) 5 0 Right (Animation 0 1500 False True) (Animation 0 800 False True)
+            , Enemy ( 400, 200 ) 5 0 Left (Animation 0 1500 False True) (Animation 0 1000 False True)
+            ]
